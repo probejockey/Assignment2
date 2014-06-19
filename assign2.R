50a874c1ccb0c5b59609a50714d9fb8f8b4e4d09
probejockey <glennpoirier@gmail.com>
2014-06-19 10:31:59 -0400

    triL

--- assign2.R
+++ assign2.R
@@ -1,11 +1,14 @@
 makeCacheMatrix <- function(x = numeric()) {
- #makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+ #makeCacheMatrix: This function creates a special "matrix" 
+ #object that can cache its inverse.
   
 }
 
 cacheSolve <- function(x){
-#   cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), 
-#   then the cachesolve should retrieve the inverse from the cache.
+# cacheSolve: This function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already been 
+#calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
   
   
 }
