4afd60e4326ff5f064d311c8e6b8c58ef042b7e5
probejockey <glennpoirier@gmail.com>
2014-06-19 15:58:00 -0400

    working?

--- assign2.R
+++ assign2.R
@@ -2,13 +2,48 @@ makeCacheMatrix <- function(x = numeric()) {
  #makeCacheMatrix: This function creates a special "matrix" 
  #object that can cache its inverse.
   
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolv <- function(sol) m <<- sol
+  getsolv <- function() m
+  list(set = set, get = get, setsolv = setsolv, getsolv = getsolv)
+  
+  
+
+  
 }
 
 cacheSolve <- function(x){
 # cacheSolve: This function computes the inverse of the special "matrix" 
-# returned by makeCacheMatrix above. If the inverse has already been 
+# returned by makeCacheMatrix above. If the inverse t has already been 
 #calculated (and the matrix has not changed), 
 # then the cachesolve should retrieve the inverse from the cache.
+  print("incachsolve")
   
+  m <- x$getsolv()
+  
+  print("after  get")
+  
+  
+  print(m)
+  
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  #v <- x$get()
+  #print(x$get())
+  
+  m <- solve(x$get())
+  print("printing m")
+  print(m)
+  x$setsolv(m)
+  m
   
 }
